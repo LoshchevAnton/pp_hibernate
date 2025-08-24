4399d1b578d2705036e1418e0acfcfccfd576443
LoshchevAnton <loweva88@gmail.com>
2025-08-24 17:35:09 +0530

    Committing solution

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,25 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import javax.persistence.Query;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final String CREATE_TABLE =
+            """
+            CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(250) NOT NULL,
+            lastName VARCHAR(250) NOT NULL,
+            age INT NOT NULL)
+            """;
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS users";
+    private static final String DELETE_ALL = "DELETE FROM users";
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +27,67 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (SessionFactory factory = Util.getSessionFactory();
+             Session session = factory.openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery(CREATE_TABLE);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (SessionFactory factory = Util.getSessionFactory();
+             Session session = factory.openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery(DROP_TABLE);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (SessionFactory factory = Util.getSessionFactory();
+            Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (SessionFactory factory = Util.getSessionFactory();
+             Session session = factory.openSession()) {
+            session.beginTransaction();
+            User user = new User();
+            user.setId(id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (SessionFactory factory = Util.getSessionFactory();
+             Session session = factory.openSession()) {
+            session.beginTransaction();
+            var result = session.createQuery("SELECT a from User a", User.class).list();
+            session.getTransaction().commit();
+            return result;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (SessionFactory factory = Util.getSessionFactory();
+             Session session = factory.openSession()) {
+            session.beginTransaction();
+            Query query = session.createSQLQuery(DELETE_ALL);
+            query.executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -16,5 +25,28 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USER);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            settings.put(Environment.FORMAT_SQL, "true");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(settings).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return sessionFactory;
     }
 }
